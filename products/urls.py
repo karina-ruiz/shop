ae1b156cd7d3cb3b32b83d3ea9205c46ae6c371b
Karina Ruiz Marcial <marcialkarina48@gmail.com>
2024-09-09 13:46:25 -0600

    hice un formulario

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .view import ProductFormView
+from .views import ProductFormView, ProductListView
 
 urlpatterns = [
+    path ("", ProductListView.as_view(), name="list_product"),
     path('agregar/', ProductFormView.as_view(), name="add_product"),
-
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,20 @@
-from django.views import generic
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views import generic 
 from .forms import ProductForm
+
 class ProductFormView(generic.FormView):
     template_name = "products/add_product.html"
     form_class = ProductForm
+    success_url = reverse_lazy("add_product")
+
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+
+class ProductListView(generic.ListView):
+    model = 
+    template_name = "products/list_product.html"
+    context_object_name = 
